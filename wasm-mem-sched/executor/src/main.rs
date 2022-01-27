e1473f89f73ec72d04a6382633664e0e03d347da
Shota Nakagawa <g21210368a@edu.teu.ac.jp>
2022-01-27 16:13:22 +0900

    Fix the finalize

--- wasm-mem-sched/executor/src/main.rs
+++ wasm-mem-sched/executor/src/main.rs
@@ -1,7 +1,9 @@
 use crate::config::{Config, ModuleKind};
 use clap::Parser;
+use futures::future::join_all;
 use sha2::{Digest, Sha256};
 use std::{
+    borrow::BorrowMut,
     collections::HashMap,
     io,
     path::{Path, PathBuf},
@@ -11,10 +13,10 @@ use std::{
 use tokio::{
     process::Command,
     signal::ctrl_c,
-    sync::{mpsc, oneshot, Mutex},
+    sync::{mpsc, oneshot},
     task::JoinHandle,
 };
-use tracing::info;
+use tracing::{error, info};
 use uuid::Uuid;
 
 mod config;
@@ -64,7 +66,7 @@ impl InstanceSpec {
 #[derive(Debug)]
 pub struct Instance {
     pub spec: InstanceSpec,
-    pub handler: JoinHandle<anyhow::Result<()>>,
+    pub handler: Option<JoinHandle<anyhow::Result<()>>>,
     pub stop_cmd_sender: Option<oneshot::Sender<()>>,
 }
 
@@ -107,6 +109,7 @@ impl Instance {
                     Ok(())
                 },
                 _ = rx => {
+                    info!("got stop message ({})", spec.uid);
                     child.kill().await?;
                     status_sender.send((spec.uid, InstanceStatus::Quit(child.wait().await?))).await?;
                     Ok(())
@@ -116,7 +119,7 @@ impl Instance {
 
         Ok(Self {
             spec,
-            handler,
+            handler: Some(handler),
             stop_cmd_sender: Some(tx),
         })
     }
@@ -147,7 +150,7 @@ async fn main() -> anyhow::Result<()> {
     let conf = Config::from_path(Path::new(&cli.config)).await?;
 
     let mgr_handle: JoinHandle<anyhow::Result<()>> = tokio::spawn(async move {
-        let instance_collector = Arc::new(Mutex::new(HashMap::<Uuid, Instance>::new()));
+        let mut instance_collector = HashMap::<Uuid, Instance>::new();
         let (status_sender, mut status_reciever) = mpsc::channel::<(Uuid, InstanceStatus)>(100);
         for c in conf.entries.iter() {
             let module = Arc::new(Module::new(c.kind, &c.path).await?);
@@ -156,10 +159,7 @@ async fn main() -> anyhow::Result<()> {
                 let tx = status_sender.clone();
                 let instance = Instance::spawn(spec, tx).await?;
 
-                instance_collector
-                    .lock()
-                    .await
-                    .insert(instance.spec.uid, instance);
+                instance_collector.insert(instance.spec.uid, instance);
             }
         }
         loop {
@@ -167,12 +167,33 @@ async fn main() -> anyhow::Result<()> {
                 status = status_reciever.recv() => {
                     let (uid, status) = status.unwrap();
                     info!("status({:?}) update recieved {:?}", status,uid);
-                    instance_collector.lock().await.get_mut(&uid).unwrap().spec.status = status;
+                    instance_collector.get_mut(&uid).unwrap().spec.status = status;
                 },
                 _ = ctrl_c() => {
-                    info!("Got ctrl_c. instances: {:#?}", instance_collector.lock().await);
+                    info!("Got ctrl_c. instances: {:?}", instance_collector);
+                    for (uid, instance) in instance_collector.borrow_mut() {
+                        let sender = instance.stop_cmd_sender.take();
+                        if let Some(sender) = sender {
+                            if let Err(e) = sender.send(()).map_err(|_| {
+                                anyhow::anyhow!("the stop message will never be received. (UID: {})", uid)
+                            }){
+                                info!("{:?}", e);
+                            };
+                        }
+                    }
+                    join_all(instance_collector.iter_mut().filter_map(|(_uid, i)| i.handler.take())).await
+                        .iter()
+                        .for_each(|r| match r {
+                            Ok(Ok(_)) => {}
+                            Ok(Err(e)) => {
+                                error!("{:?}", e);
+                            }
+                            Err(e) => {
+                                error!("{:?}", e);
+                            }
+                        });
                     break;
-                }
+                },
             }
         }
         Ok(())
