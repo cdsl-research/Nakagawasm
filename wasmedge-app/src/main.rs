69e4ef49bc61b3b9a4e8960876a015fbdc4502a3
Shota Nakagawa <g21210368a@edu.teu.ac.jp>
2022-04-27 08:15:28 +0000

    Add fileserver and support urlencoding

--- wasmedge-app/src/main.rs
+++ wasmedge-app/src/main.rs
@@ -8,16 +8,59 @@ use httpcodec::{
     BodyEncoder, HttpVersion, ReasonPhrase, Request, RequestDecoder, Response, ResponseEncoder,
     StatusCode,
 };
-use std::io::{Read, Write};
+use std::{
+    fs::File,
+    io::{Read, Write},
+    path::PathBuf,
+};
 use wasmedge_wasi_socket::{Shutdown, TcpListener, TcpStream};
 
-fn handle_http(req: Request<Vec<u8>>) -> bytecodec::Result<Response<Vec<u8>>> {
-    Ok(Response::new(
-        req.http_version(),
-        StatusCode::new(200)?,
-        ReasonPhrase::new("")?,
-        req.body().clone(),
-    ))
+fn file_server(path: &str) -> Option<Vec<u8>> {
+    let mut target = PathBuf::from(format!(".{}", urlencoding::decode(path).unwrap()));
+    println!("target: {:?}", target);
+    if !target.exists() {
+        println!("does not exists");
+        None
+    } else {
+        if target.is_dir() {
+            target.push("index.html");
+        }
+
+        println!("target: {:?}", target);
+        if target.is_file() {
+            let mut f = File::open(target.as_path()).unwrap();
+            let mut buf = vec![];
+            f.read_to_end(&mut buf).unwrap();
+            Some(buf)
+        } else {
+            None
+        }
+    }
+}
+
+fn handle_http(req: Request<Vec<u8>>) -> Response<Vec<u8>> {
+    match req.method().as_str() {
+        "GET" => match file_server(req.request_target().as_str()) {
+            Some(v) => Response::new(
+                req.http_version(),
+                StatusCode::new(200).unwrap(),
+                ReasonPhrase::new("").unwrap(),
+                v,
+            ),
+            None => Response::new(
+                req.http_version(),
+                StatusCode::new(404).unwrap(),
+                ReasonPhrase::new("NOT FOUND").unwrap(),
+                b"404 Not Found".to_vec(),
+            ),
+        },
+        _ => Response::new(
+            req.http_version(),
+            StatusCode::new(500).unwrap(),
+            ReasonPhrase::new("NOT ALLOWED HTTP METHOD").unwrap(),
+            Vec::with_capacity(0),
+        ),
+    }
 }
 
 /// streamからHTTPリクエストのヘッダーとボディの全てを受けとって返す
@@ -38,7 +81,7 @@ fn recv_all(stream: &mut TcpStream) -> std::io::Result<Vec<u8>> {
                     if let Some(h) = req.headers.iter().find(|h| h.name.eq("Content-Length")) {
                         std::str::from_utf8(h.value).unwrap().parse().unwrap()
                     } else {
-                        panic!("cannot find header `Content-Length`");
+                        break;
                     };
 
                 if content_length == data.len() - body_offset {
@@ -59,7 +102,7 @@ fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
     let mut decoder = RequestDecoder::<httpcodec::BodyDecoder<RemainingBytesDecoder>>::default();
 
     let req = match decoder.decode_from_bytes(data.as_slice()) {
-        Ok(req) => handle_http(req),
+        Ok(req) => Ok(handle_http(req)),
         Err(e) => Err(e),
     };
 
@@ -70,7 +113,7 @@ fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
             Response::new(
                 HttpVersion::V1_1,
                 StatusCode::new(500).unwrap(),
-                ReasonPhrase::new(err.clone().as_str()).unwrap(),
+                ReasonPhrase::new(&e.to_string()).unwrap(),
                 err.into_bytes(),
             )
         }
